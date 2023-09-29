7f7b72f0f2be323d915269dbef5402fae6b96bb4
Solomia Bobyak <mia.bobyak@gmail.com>
2023-09-29 19:56:54 +0300

    Add gulp plugins

--- gulpfile.js
+++ gulpfile.js
@@ -1,58 +1,71 @@
-var gulp = require ("gulp");
-//додаткові плагіни Gulp
-var sass = require ("gulp-sass"); //конвертує SASS в CSS
- cssnano = require ("gulp-cssnano"); //мінімізація CSS
- autoprefixer = require ('gulp-autoprefixer'); //додавання префіксів в
- //CSS для підтримки
- //старих браузерів
- imagemin = require ('gulp-imagemin'); //стиснення зображень
- concat = require ("gulp-concat"); //об'єднання файлів - конкатенація
- uglify = require ("gulp-uglify"); //мінімізація javascript
- rename = require ("gulp-rename"); //перейменування файлів
+const { src, dest, watch, series } = require("gulp");
+const concat = require("gulp-concat");
+const sass = require("gulp-sass")(require('sass'));
+const autoprefixer = require("gulp-autoprefixer");
+const rename = require("gulp-rename");
+const cssnano = require("gulp-cssnano");
+const uglify = require("gulp-uglify");
+const imagemin = require("gulp-imagemin");
 
- //копіювання HTML файлів в папку dist
-gulp.task ( "html", function () {
-  return gulp.src ( "src / *. html")
-  .pipe (gulp.dest ( "dist"));
- });
- //об'єднання, компіляція Sass в CSS, додавання префіксів і подальша мінімізація коду
- gulp.task ( "sass", function () {
-  return gulp.src ( "src / sass / *. sass")
-  .pipe (concat ( 'styles.sass'))
-  .pipe (sass ())
-  .pipe (autoprefixer ({
-  browsers: [ 'last 2 versions'],
-  cascade: false
+function simpleTask() {
+  console.log('This is a test task!');
+}
+exports.default = simpleTask
+
+//копіювання HTML файлів в папку dist
+function task_html () {
+  return src("app/*.html")
+  .pipe(dest("dist"));
+}
+exports.html = task_html
+
+//об'єднання, компіляція Sass в CSS, додавання префіксів і подальша мінімалізація коду
+function task_sass() {
+  return src("app/sass/*.sass")
+  .pipe(concat('styles.sass'))
+  .pipe(sass())
+  .pipe(autoprefixer ({
+    browsers: ['last 2 versions'],
+    cascade: false
   }))
-  .pipe (cssnano ())
-  .pipe (rename ({suffix: '.min'}))
-  .pipe (gulp.dest ( "dist / css"));
- });
- //об'єднання і стиснення JS-файлів
- gulp.task ( "scripts", function () {
-  return gulp.src ( "src / js / *. js") //вихідна директорія файлів
-  .pipe (concat ( 'scripts.js')) // конкатенація js-файлів в один
-  .pipe (uglify ()) //стиснення коду
-  .pipe (rename ({suffix: '.min'})) //перейменування файлу з
-  //приставкою .min
-  .pipe (gulp.dest ( "dist / js")); // директорія продакшена
- });
- //cтискання зображень
- gulp.task ( 'imgs', function () {
-  return gulp.src ( "src / images /*.+ (jpg | jpeg | png | gif)")
-  .pipe (imagemin ({
-  progressive: true,
-  svgoPlugins: [{removeViewBox: false}],
-  interlaced: true
+  .pipe(cssnano())
+  .pipe(rename({suffix:'.min'}))
+  .pipe(dest("dist/css"));
+} 
+exports.sass = task_sass
+
+//об'єднання і стискання JS-файлів
+function task_scripts() {
+  return src("app/js/*.js")//вихідна директорія файлів
+  .pipe(concat('scripts.js'))//конкатенація js-файлів в один
+  .pipe(uglify())//стиснення коду
+  .pipe(rename({suffix:'.min'}))//перейменування файлу з приставкою .min
+  .pipe(dest("dist/js"));//директорія продакшена
+}
+exports.scripts = task_scripts
+
+//стискання зображень
+function task_imgs() {
+  return src("app/img/*.+(jpg|jpeg|png|gif)")
+  .pipe(imagemin({
+    progressive: true,
+    svgoPlugins: [{removeViewBox: false}],
+    interlaced: true
   }))
-  .pipe (gulp.dest ( "dist / images"))
- });
- //відстежування за змінами у файлах
- gulp.task ( "watch", function () {
-  gulp.watch ( "src / *. html", [ "html"]);
-  gulp.watch ( "src / js / *. js", [ "scripts"]);
-  gulp.watch ( "src / sass / *. sass", [ "sass"]);
-  gulp.watch ( "src / images /*.+ (jpg | jpeg | png | gif)", [ "imgs"]);
- });
- //Запуск тасків за замовчуванням
- gulp.task ( "default", [ "html", "sass", "scripts", "imgs", "watch"]);
+  .pipe(dest("dist/images"))
+}
+exports.imgs = task_imgs
+
+//відстежування за змінами у файлах
+function task_watch() {
+  watch("app/*.html",task_html);
+  watch("app/js/*.js",task_scripts);
+  watch("app/sass/*.sass",task_sass);
+  watch("app/images/*.+(jpg|jpeg|png|gif)",task_imgs);
+}
+exports.watch = task_watch
+
+//запуск тасків за замовчуванням
+// exports.build = build;
+exports.default = series(task_html,task_sass,task_scripts,task_imgs,
+  task_watch);
